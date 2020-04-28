b5e28b2466d6d430f68e33f44904f46a4d13d88e
wkStark <402983305@qq.com>
2020-04-28 21:06:17 +0800

    第一个xlua使用的场景
    简单输出一个hello c#在unity控制台打印

---
+++ XLuaLearn_Client/Assets/Scenes/001-HelloLua/HelloCSharp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;               ////第一步导入XLua命名空间
+public class HelloCSharp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LuaEnv luaEnv = new LuaEnv();           //第二步创建Lua虚拟机
+        luaEnv.DoString("print('hello C#')");    //DoString函数，里面写lua脚本，具体参见官方API说明
+        luaEnv.Dispose();                       //lua虚拟机的释放
+    }
+}
