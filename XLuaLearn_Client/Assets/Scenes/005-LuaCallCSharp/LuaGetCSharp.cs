d0f1399c5380babd65cc3c2bd58d19716011486c
wkStark <402983305@qq.com>
2020-05-05 14:14:34 +0800

    .gitignore now wordk

---
+++ XLuaLearn_Client/Assets/Scenes/005-LuaCallCSharp/LuaGetCSharp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+public class LuaGetCSharp : MonoBehaviour
+{
+    LuaEnv luaEnv;
+    public TextAsset luaScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        luaEnv = new LuaEnv();
+        luaEnv.DoString(luaScript.text); 
+    }
+
+    private void OnDestroy()
+    {
+        luaEnv.Dispose();
+    }
+}
